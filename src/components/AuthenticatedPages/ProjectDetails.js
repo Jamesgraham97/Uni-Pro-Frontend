9bc69a3e85ade22489bde4331b04e52d77f500c6
james graham <grahamjames152@gmail.com>
2024-08-02 19:08:35 +0100

    Fix ESLint error and update GitHub Actions configuration

--- src/components/AuthenticatedPages/ProjectDetails.js
+++ src/components/AuthenticatedPages/ProjectDetails.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { useParams, Link } from 'react-router-dom';
+import { useParams } from 'react-router-dom';
 import { Form, Button, Modal, Container, Row, Col, ListGroup, Image } from 'react-bootstrap';
 import { FaEdit } from 'react-icons/fa';
 import { MdDelete } from 'react-icons/md';
